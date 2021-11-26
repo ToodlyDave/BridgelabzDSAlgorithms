cfa99ab6883e652bd22ef178bea5359841d7f0c7
ToodlyDave <alapatdavid@gmail.com>
2021-11-26 08:22:09 +0530

    CREATE bubble sort program

---
+++ src/com/problems/BubbleSort.java
@@ -0,0 +1,20 @@
+package com.problems;
+
+public class BubbleSort {
+
+	public int[] sort(int [] array) {
+		int temp;
+		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length - 1; j++) {
+				if (array[j] > array[j + 1]) {
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+		
+		return array;
+	}
+}
---
+++ src/com/problems/TestClass.java
@@ -0,0 +1,39 @@
+package com.problems;
+
+import java.util.Scanner;
+
+public class TestClass {
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	public static int[] getArray() {
+		System.out.print(" Please enter the size of the array: ");
+		int n = scan.nextInt();
+		
+		int array[] = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print(" Please enter the " + (i + 1) + " value: ");
+			array[i] = scan.nextInt();
+		}
+		
+		return array;
+	}
+	
+	public static void display(int [] array) {
+		for (int i = 0; i < array.length; i++)
+			System.out.print(" " + array[i]);
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BubbleSort sort = new BubbleSort();
+		int array[] = getArray();
+		
+		sort.sort(array);
+		display(array);
+	}
+
+}
