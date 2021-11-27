f4be722ae647ab682883848d1f5689040ed5ecac
ToodlyDave <alapatdavid@gmail.com>
2021-11-27 23:09:56 +0530

    CREATE a program to perform binary search

--- src/com/problems/TestClass.java
+++ src/com/problems/TestClass.java
@@ -1,12 +1,17 @@
 package com.problems;
 
+import java.util.Arrays;
+
 public class TestClass {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		FindPrime obj = new FindPrime();
-		obj.prime();
+		String[] array = {"hi", "there", "these", "are", "random", "words", "in", "an", "array"};
+		BinarySearch binary = new BinarySearch();
+		Arrays.sort(array);
+		
+		binary.binarySearch(array, 0, array.length, "hi");
 	}
 
 }
