8d3326bc0321317bc4f206ba5faee5cefb6c1b00
ToodlyDave <alapatdavid@gmail.com>
2021-12-06 12:13:38 +0530

    CREATE program to check if the given string is palindrome

--- src/com/dataStructures/DataStructureTest.java
+++ src/com/dataStructures/DataStructureTest.java
@@ -2,39 +2,21 @@ package com.dataStructures;
 
 import java.util.Scanner;
 
-import com.dataStructures.hashTable.HashTable;
+import com.dataStructures.palindrome.PalindromeChecker;
 
 public class DataStructureTest {
 
 	public static void main(String[] args) {
 
-		HashTable hash = new HashTable();
 		Scanner scan = new Scanner(System.in);
-		int ch;
-		hash.initializeHashTable();
 
-		while (true) {
-			System.out.println("\n 1. Search\n 2. Display\n 3. Finish Up");
-			System.out.print(" Please enter your choice: ");
-			ch = scan.nextInt();
+		System.out.print(" Please enter the string to check: ");
+		String word = scan.next();
 
-			switch (ch) {
-			case 1:
-				System.out.print(" Please enter the number to search: ");
-				int n = scan.nextInt();
-				hash.search(n);
-				break;
-			case 2:
-				hash.display();
-				break;
-			case 3:
-				hash.writeIntoFile();
-				scan.close();
-				return;
-			default:
-				System.out.println(" Please enter a valid choice!");
-			}
-		}
+		PalindromeChecker check = new PalindromeChecker(word.length());
+		check.checkPalindrome(word);
+
+		scan.close();
 
 	}
 }
